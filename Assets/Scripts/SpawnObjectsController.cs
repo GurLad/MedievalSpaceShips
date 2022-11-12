31933afe61c53210b4c8d47866ce0f7cb64146c7
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2022-11-12 20:25:08 +0100

    Added more events

--- Assets/Scripts/SpawnObjectsController.cs
+++ Assets/Scripts/SpawnObjectsController.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class SpawnObjectsController : MonoBehaviour
 {
-
-    [SerializeField] GameObject Eel;
-    [SerializeField] GameObject Rock1;
-    [SerializeField] GameObject Rock2;
-    [SerializeField] GameObject Rock3;
+    public Vector2 Rate;
+    public List<SpawnableObject> Objects;
+    private float count = 0;
+    private int totalCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (SpawnableObject obj in Objects)
+        {
+            totalCount += obj.Chance;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (count <= 0)
+        {
+            int result = Random.Range(1, totalCount);
+            foreach (SpawnableObject obj in Objects)
+            {
+                result -= obj.Chance;
+                if (result <= 0)
+                {
+                    SpawnRandomObject(obj.Object);
+                }
+            }
+        }
     }
 
     void SpawnRandomObject(GameObject obj)
     {
         Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Random.Range(0,Screen.height), Camera.main.farClipPlane/2));
-        Instantiate(obj, screenPosition, Quaternion.identity);
+        GameObject newObj = Instantiate(obj, screenPosition, Quaternion.identity);
+        newObj.AddComponent<ObjectMover>();
     }
+}
+
+[SerializeField]
+public class SpawnableObject
+{
+    public GameObject Object;
+    public int Chance;
 }
